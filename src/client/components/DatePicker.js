c5adc2c30a8a107af03e2122227d0228d91f6915
Martti Laine <martti@codeclown.net>
2020-11-09 15:51:17 +0200

    Change from boring-datepicker to native-datepicker

--- src/client/components/DatePicker.js
+++ src/client/components/DatePicker.js
@@ -1,9 +1,9 @@
-import BoringDatepicker from "boring-datepicker/src/react";
+import NativeDatepicker from "native-datepicker/src/react";
 import { createElement as h } from "react";
 
 const DatePicker = ({ value, onChange, images }) => {
   return h(
-    BoringDatepicker,
+    NativeDatepicker,
     { value, onChange, className: "DatePicker" },
     h("img", { src: images.calendar })
   );
